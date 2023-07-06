0cebb182d2847d9226537d547ddd7686761c9ef4
gnarus-tm <71601039+gna-rus@users.noreply.github.com>
2023-07-06 12:35:20 +0300

    add interFace

--- Task.java
+++ Task.java
@@ -1,41 +1,72 @@
+//Формат сдачи: ссылка на подписанный git-проект.
+
+//Задание
+
+//Реализуйте структуру телефонной книги с помощью HashMap.
+//Программа также должна учитывать, что во входной структуре будут 
+//повторяющиеся имена с разными телефонами, 
+//их необходимо считать, как одного человека с разными телефонами. 
+//Вывод должен быть отсортирован по убыванию числа телефонов.
+
+
+  //passportsAndNames.put("Дональд Джон Трамп", 8082771);
+  //int lidiaName = passportsAndNames.get("Лидия");
+  //passportsAndNames.remove("Дональд Джон Трамп");
+
 import java.util.*;
+import java.util.Scanner;
+
+
 
 public class Main
 {
+    public static void print_of_nums(HashMap Clients){
+    System.out.println("Все контакты");
+    Set<String> keys = Clients.keySet();
+    System.out.println(keys);
+}
+    public static HashMap add_client(HashMap Clients)
+    {
+        
+        return Clients;
+    }
+
+
+    
 	public static void main(String[] args) {
 	    
-		HashMap<String, Integer> passportsAndNames = new HashMap<>();
-	   passportsAndNames.put("Лидия", 212133);
-       passportsAndNames.put("Иван Михайлович Серебряков", 162348);
-       passportsAndNames.put("Дональд Джон Трамп", 8082771);
-       System.out.println(passportsAndNames);
-       
-       int lidiaName = passportsAndNames.get("Лидия");
-   System.out.println(lidiaName);
-
-
-   passportsAndNames.remove("Дональд Джон Трамп");
-   System.out.println(passportsAndNames);
-   
-///////////////////////////   
-   
-   
-   int[] abc = new int []{1,2,3,4};
-   HashMap<String, int[]> test1 = new HashMap<>();
-   test1.put("Test1", abc);
-   System.out.println(test1);
-       
- ////////////////////////////     
-    LinkedList<String> earlBio = new LinkedList<>();
-    String str1 = new String("Hello World!");
-       String str2 = new String("My name is Earl");
+	System.out.println("Меню: \n1 - Вывести список всех контактов;\n2 - Добавить клиента и его номер телефона к базе дынных");
+	System.out.println("3 - Списко всех клиентов и их номеров телефонов;\n4 - Выход");
     
-       earlBio.add(str1);
-       earlBio.add(str2);
+
+    LinkedList<String> NumPhon1 = new LinkedList<>();
+    String str1 = new String("8(965)643-21-22");
+    String str2 = new String("8(965)633-11-99");
+    NumPhon1.add(str1);
+    NumPhon1.add(str2);
        
-       HashMap<String, LinkedList> test2 = new HashMap<>();
-   test2.put("Test2", earlBio);
-   System.out.println(test2);
+    HashMap<String, LinkedList> Clients = new HashMap<>();
+   Clients.put("Иванов Иван", NumPhon1);
+   System.out.println(Clients);
+   
+    int num = 0;
+    while (num != 4){
+    System.out.print("Введите число: ");
+   	Scanner scanner = new Scanner(System.in);
+    num = scanner.nextInt();
+    if (num == 1)
+    {
+        print_of_nums(Clients);
+    }
+    
+    if (num == 2)
+    {
+        Clients = add_client(Clients);
+    }
+    
+    
+    }
+    
        
 	}
 }
